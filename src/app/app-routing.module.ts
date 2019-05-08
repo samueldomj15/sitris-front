95eb56530f5843d4f8af380c680c973be5214e8c
Samuel Dominguez <samueldomj15@gmail.com>
2019-05-07 23:29:45 -0500

    Template listo con rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { ModulosComponent } from './pages/modulos/modulos.component';
+import { RecepcionComponent } from './pages/recepcion/recepcion.component';
+
+const routes: Routes = [
+  {path: 'inicio', component: ModulosComponent},
+  {path: 'recepcion', component: RecepcionComponent},
+  {path: '**', pathMatch: 'full', redirectTo: 'inicio'}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Rutas
 import { AppRoutingModule } from './app-routing.module';
+
+// Componentes
 import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { BreadcrumbsComponent } from './components/breadcrumbs/breadcrumbs.component';
+import { ModulosComponent } from './pages/modulos/modulos.component';
+import { RecepcionComponent } from './pages/recepcion/recepcion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NavbarComponent,
+    SidebarComponent,
+    FooterComponent,
+    BreadcrumbsComponent,
+    ModulosComponent,
+    RecepcionComponent,
   ],
   imports: [
     BrowserModule,
